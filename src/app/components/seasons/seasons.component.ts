ce9c0e323bd5e5428d0cd081aedc81ea4680d12c
Chris Snow <cgsnow@live.com>
2023-03-08 17:24:32 -0700

    Initial commit

---
+++ src/app/components/seasons/seasons.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-seasons',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      seasons works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SeasonsComponent {
+
+}

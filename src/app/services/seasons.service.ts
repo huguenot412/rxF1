7b8f5f6d44920ca6eeae0c686420ad21359595f4
Chris Snow <cgsnow@live.com>
2023-03-14 13:20:53 -0600

    Create separate requests for each endpoint

--- src/app/services/seasons.service.ts
+++ src/app/services/seasons.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, share } from 'rxjs';
+import { map, Observable, share } from 'rxjs';
 import { ERGAST_API_BASE, RESPONSE_FORMAT, SERIES } from '../consts/ergast-api';
 import { DataSets } from '../enums/data-sets';
 import { RouteParams } from '../enums/route-params';
+import { DriversResponse } from '../models/drivers-response';
 import { GetSeasonsConfig } from '../models/get-seasons-config';
-import { SeasonData } from '../models/season';
+import { QualifyingResponse } from '../models/qualifying-response';
+import { ResultsResponse } from '../models/results-response';
+import { StandingsResponse } from '../models/standings-response';
 
 @Injectable({
   providedIn: 'root',
@@ -23,39 +26,38 @@ export class SeasonsService {
     share()
   );
 
-  public getSeasonData(config: GetSeasonsConfig) {
-    const { year, dataSet, limit, offset } = config;
+  public getDrivers(config: GetSeasonsConfig): Observable<DriversResponse> {
+    const { year, limit, offset } = config;
 
-    return this._http
-      .get<SeasonData>(
-        `${ERGAST_API_BASE}/${SERIES}/${year}/${dataSet}${RESPONSE_FORMAT}?limit=${limit}&offset=${offset}`
-      )
-      .pipe(
-        map((data: any) => {
-          let results: SeasonData = {
-            data: [],
-            total: data.MRData.total,
-          };
+    return this._http.get<DriversResponse>(
+      `${ERGAST_API_BASE}/${SERIES}/${year}/drivers${RESPONSE_FORMAT}?limit=${limit}&offset=${offset}`
+    );
+  }
 
-          switch (dataSet) {
-            case DataSets.Drivers:
-              results.data = data.MRData.DriverTable.Drivers;
-              break;
-            case DataSets.Standings:
-              results.data =
-                data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-              break;
-            case DataSets.Qualifying:
-              results.data = data.MRData.RaceTable.Races[0].QualifyingResults;
-              break;
-            case DataSets.Results:
-              results.data = data.MRData.RaceTable.Races;
-              break;
-          }
+  public getResults(config: GetSeasonsConfig): Observable<ResultsResponse> {
+    const { year, limit, offset } = config;
 
-          return results;
-        })
-      );
+    return this._http.get<ResultsResponse>(
+      `${ERGAST_API_BASE}/${SERIES}/${year}/results${RESPONSE_FORMAT}?limit=${limit}&offset=${offset}`
+    );
+  }
+
+  public getQualifying(
+    config: GetSeasonsConfig
+  ): Observable<QualifyingResponse> {
+    const { year, limit, offset } = config;
+
+    return this._http.get<QualifyingResponse>(
+      `${ERGAST_API_BASE}/${SERIES}/${year}/qualifying${RESPONSE_FORMAT}?limit=${limit}&offset=${offset}`
+    );
+  }
+
+  public getStandings(config: GetSeasonsConfig): Observable<StandingsResponse> {
+    const { year, limit, offset } = config;
+
+    return this._http.get<StandingsResponse>(
+      `${ERGAST_API_BASE}/${SERIES}/${year}/driverStandings${RESPONSE_FORMAT}?limit=${limit}&offset=${offset}`
+    );
   }
 }
 

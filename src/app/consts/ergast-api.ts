d687e3d69de0c74280149ca5e06ea3b32cd4eb3a
Chris Snow <cgsnow@live.com>
2023-03-19 17:40:46 -0600

    Https

--- src/app/consts/ergast-api.ts
+++ src/app/consts/ergast-api.ts
@@ -1,3 +1,3 @@
-export const ERGAST_API_BASE = 'http://ergast.com/api';
+export const ERGAST_API_BASE = 'https://ergast.com/api';
 export const SERIES = 'f1';
 export const RESPONSE_FORMAT = '.json';
